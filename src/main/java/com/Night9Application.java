a7d81c7ae2da5660c5fee8db98c03ff19d4b2e17
jishuai <903498494@qq.com>
2021-12-21 20:53:17 +0800

    创建YML文件 修改端口 引入Springboot核心依赖

---
+++ src/main/java/com/Night9Application.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Night9Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Night9Application.class,args);
+    }
+}
